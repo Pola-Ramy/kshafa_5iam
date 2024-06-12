39355f2602134b5dad57f8dd108d0df6fd25d4d3
Pola-Ramy <140097912+Pola-Ramy@users.noreply.github.com>
2024-06-12 21:49:52 +0300

    Add files via upload

---
+++ الخيم/script.js
@@ -0,0 +1,12 @@
+document.getElementById('openPageindex2').addEventListener('click', function() {
+    window.open('index2.html', '_blank'); // Replace 'newpage.html' with the URL of the page you want to open
+});
+document.getElementById('openPageindex3').addEventListener('click', function() {
+    window.open('index3.html', '_blank'); // Replace 'newpage.html' with the URL of the page you want to open
+});
+document.getElementById('openPageindex4').addEventListener('click', function() {
+    window.open('index4.html', '_blank'); // Replace 'newpage.html' with the URL of the page you want to open
+});
+document.getElementById('openPageindex5').addEventListener('click', function() {
+    window.open('index5.html', '_blank'); // Replace 'newpage.html' with the URL of the page you want to open
+});
